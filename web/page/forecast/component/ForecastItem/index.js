7e9fde34b5c7e381ce332568a6f26c4e4b68d43d
clark <332809037@qq.com>
2020-07-10 12:57:04 +0800

    专家预测

--- web/page/forecast/component/ForecastItem/index.js
+++ web/page/forecast/component/ForecastItem/index.js
@@ -14,6 +14,29 @@ function ForecastItem(props) {
       <img src={require("../../../../assets/images/pot.png")}/>{props.itemInfo.category}
   
     </div>
+
+
+    <div className="peopleGroup">
+      {props.itemInfo.data&&props.itemInfo.data.map((item,index) => (
+         <div className="peopleItem" key={index}>
+           {(index==0&& <div><img src={require("../../../../assets/images/one.png")}/> </div> )}
+           {(index==1&& <div><img src={require("../../../../assets/images/two.png")}/> </div> )}
+           {(index==2&& <div><img src={require("../../../../assets/images/three.png")}/> </div> )}
+           {(index==3&& <div><div className="peopleItemNumber">4</div></div> )}
+           <div> 
+              <div>
+              <img className="userIcon"  src={item.icon}/>
+              </div>
+              <div className="user_Info">
+                <div className="UserName">{item.master_name}</div>
+                <div className="UserGet">最近连中{item.recent_hit}期</div>
+              </div>
+              
+            </div>
+           <div>  近{item.recent_result}</div>
+        </div>
+      ))}
+    </div>
   </div>);
 }
 
