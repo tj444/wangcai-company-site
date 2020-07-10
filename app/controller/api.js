68eaa9e93fb8b1a7338fcf7114f3a74976107f0d
姚铭 <492467237@qq.com>
2020-07-10 23:23:06 +0800

    feat: 完成基础布局，完成server端代码编写

--- app/controller/api.js
+++ app/controller/api.js
@@ -7,7 +7,15 @@ class PageController extends Controller {
       // Page为webpack打包的chunkName，项目默认的entry为Page
       ctx.type = 'text/json';
       ctx.status = 200;
-      ctx.body = await ctx.service.api.index();
+      console.log(ctx.query)
+      ctx.body = (await this.ctx.curl("https://api.icaipiao123.com/api/v6/lotterynums/latest/all",{
+        method: 'get',
+        headers:{//自定义header
+          "User-Agent": "xWangcai Browser",
+        },
+        // data:ctx.query,
+        // dataType: 'json'
+      })).res.data
     } catch (error) {
       ctx.logger.error(error);
     }
--- web/models/page.js
+++ web/models/page.js
@@ -1,26 +1,7 @@
-const getData = async () => {
-  return Promise.resolve({
-    news: [
-      {
-        id: '1',
-        title: 'Racket v7.3 Release Notes'
-      },
-      {
-        id: '2',
-        title: 'Free Dropbox Accounts Now Only Sync to Three Devices'
-      },
-      { id: '3',
-        title: 'Voynich Manuscript Decoded by Bristol Academic'
-      },
-      { id: '4',
-        title: 'Burger King to Deliver Whoppers to LA Drivers Stuck in Traffic'
-      },
-      { id: '5',
-        title: 'How much do YouTube celebrities charge to advertise your product? '
-      }
-    ]
-  })
-}
+
+import { getAllLottery } from '../services/page';
+
+
 export default {
   namespace: 'page',
   state: {
@@ -30,13 +11,13 @@ export default {
     init (state, { payload }) {
       return {
         ...state,
-        news: payload.news
       }
     }
   },
   effects: {
     * getData ({ payload }, { call, put }) {
-      const data = yield call(getData)
+      const data = yield call(getAllLottery)
+      console.log(data,'@@data')
       yield put({
         type: 'init',
         payload: data
